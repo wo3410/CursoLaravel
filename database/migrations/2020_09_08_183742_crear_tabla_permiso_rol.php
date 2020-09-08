da7948027fb541d5641edb92599e911baa46a0cb
wo3410 <martingavi159@gmai.com>
2020-09-08 14:20:48 -0500

    se crearon los archivos de migracion

---
+++ database/migrations/2020_09_08_183742_crear_tabla_permiso_rol.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CrearTablaPermisoRol extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permiso_rol', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedBigInteger('rol_id');
+            $table->foreign('rol_id', 'fk_permisorol_usuario')->references('id')->on('rol')->onDelete('restrict')->onUpdate('restrict');
+            
+            $table->unsignedBigInteger('permiso_id');
+            $table->foreign('permiso_id', 'fk_permisorol_usuario')->references('id')->on('usuario')->onDelete('restrict')->onUpdate('restrict');
+            
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permiso_rol');
+    }
+}
